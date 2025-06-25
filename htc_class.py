3bb0e7b67b129962543acf1f8356a6fb211c1a0a
FoleyLab <jayfoleyiv@gmail.com>
2025-06-25 09:19:02 -0400

    some updates

--- htc_class.py
+++ htc_class.py
@@ -1,51 +1,186 @@
 import numpy as np
 from qutip import *
 
-class MultiPartiteSystem:
-    def __init__(self,
-                 w_q1, w_q2,               # qubit frequencies
-                 w_ho1, w_ho2, w_hoc,      # oscillator frequencies
-                 g_q1_ho1, g_q2_ho2,       # individual couplings
-                 g_q1_hoc, g_q2_hoc,       # common oscillator couplings
-                 N_ho=10):                 # oscillator truncation level
-        self.N_ho = N_ho
+class HolsteinTavisCummings:
+    def __init__(self, params):
+        # Required parameters
+        self.w_q1 = params['w_q1']
+        self.w_q2 = params['w_q2']
+        self.w_vib1 = params['w_vib1']
+        self.w_vib2 = params['w_vib2']
+        self.w_cav = params['w_cav']
+        self.S_1 = params['S_1']
+        self.S_2 = params['S_2']
+        self.lambda_1 = params['lambda_1']
+        self.lambda_2 = params['lambda_2']
 
-        # Operators for each subsystem
+        # Optional parameters with defaults
+        self.N_vib = params.get('N_vib', 10)
+        self.N_cav = params.get('N_cav', 10)
+        self.qubit_1_dipole_moments = params.get('qubit_1_dipole_moments', {'mu_g': 0, 'mu_e': 0, 'mu_eg': 1})
+        self.qubit_2_dipole_moments = params.get('qubit_2_dipole_moments', {'mu_g': 0, 'mu_e': 0, 'mu_eg': 1})
+        self.pauli_fierz = params.get('pauli_fierz', False)
+        self.dse_term = params.get('dse_term', False)
+        self.simple_g = params.get('simple_g', False)
+        self.g1 = params.get('g1', 0.1)  # coupling strength for qubit 1
+        self.g2 = params.get('g2', 0.1)  # coupling
+
+
+        
+        # Operators for qubit sub systems
         self.sx = sigmax()
         self.sy = sigmay()
         self.sz = sigmaz()
-        self.sm = destroy(2)   # qubit lowering operator
+        self.sm = destroy(2) # qubit lowering operator
+        self.sp = self.sm.dag() # qubit raising operator
 
-        self.a = destroy(N_ho) # oscillator lowering operator
+        # Operators for vibrational modes
+        self.a = destroy(self.N_vib) # vibrational lowering operator
+        self.ap = self.a.dag() # vibrational raising operator
+
+        # Operators for cavity mode
+        self.b = destroy(self.N_cav)
+        self.bp = self.b.dag()
 
         # Create identities for each space
-        Iq = qeye(2)
-        Io = qeye(N_ho)
+        self.Iq = qeye(2)
+        self.Ivib = qeye(self.N_vib)
+        self.Icav = qeye(self.N_cav)
 
-        # Tensor structure:
-        # [Q1, Q2, HO1, HO2, HOc]
-        self.H0 = (
-            0.5 * w_q1 * tensor(self.sz, Iq, Io, Io, Io) +
-            0.5 * w_q2 * tensor(Iq, self.sz, Io, Io, Io) +
-            w_ho1 * tensor(Iq, Iq, self.a.dag() * self.a, Io, Io) +
-            w_ho2 * tensor(Iq, Iq, Io, self.a.dag() * self.a, Io) +
-            w_hoc * tensor(Iq, Iq, Io, Io, self.a.dag() * self.a)
-        )
+        # Note on Tensor structure:
+        # [Q1, Q2, V1, V2, Cav]
 
-        # Coupling terms
-        self.H_coupling = (
-            g_q1_ho1 * (tensor(self.sm + self.sm.dag(), Iq, self.a + self.a.dag(), Io, Io)) +
-            g_q2_ho2 * (tensor(Iq, self.sm + self.sm.dag(), Io, self.a + self.a.dag(), Io)) +
-            g_q1_hoc * (tensor(self.sm + self.sm.dag(), Iq, Io, Io, self.a + self.a.dag())) +
-            g_q2_hoc * (tensor(Iq, self.sm + self.sm.dag(), Io, Io, self.a + self.a.dag()))
-        )
 
-        self.H_total = self.H0 + self.H_coupling
+    def build_qubit_dipole_operator(self):
+        """ method to build the dipole operator on a single qubit subspace.
 
-    def get_hamiltonian(self):
-        return self.H_total
+        Args:
+            mu_g (float): ground state dipole moment
+            mu_e (float): excited state dipole moment
+            mu_eg (float): transition dipole moment
 
-    def dimension(self):
-        return self.H_total.shape
+        Returns:
+            _mu (Qobj): dipole operator for the qubit subspace
+        """
 
+        # Define the dipole operator for a single qubit subspace
+        sig_p = self.sp
+        sig_m = self.sm
+
+        self.qubit_1_dipole_operator = self.qubit_1_dipole_moments['mu_g'] * sig_p * sig_m
+        self.qubit_1_dipole_operator += self.qubit_1_dipole_moments['mu_e'] * sig_m * sig_p
+        self.qubit_1_dipole_operator += self.qubit_1_dipole_moments['mu_eg'] * (sig_p + sig_m)
+
+        self.qubit_2_dipole_operator = self.qubit_2_dipole_moments['mu_g'] * sig_p * sig_m
+        self.qubit_2_dipole_operator += self.qubit_2_dipole_moments['mu_e'] * sig_m * sig_p
+        self.qubit_2_dipole_operator += self.qubit_2_dipole_moments['mu_eg'] * (sig_p + sig_m)
+
+        # Build the full dipole operator on the full Hilbert space
+        self.dipole_operator_q1 = tensor(self.qubit_1_dipole_operator, self.Iq, self.Ivib, self.Ivib, self.Icav)
+        self.dipole_operator_q2 = tensor(self.Iq, self.qubit_2_dipole_operator, self.Ivib, self.Ivib, self.Icav)
+        self.dipole_operator = self.dipole_operator_q1 + self.dipole_operator_q2
+
+    def build_qubit_hamiltonians(self):
+        """" Method to build the qubit 1 and qubit 2 Hamiltonians on the full Hilbert space."""
+        self.H_q1_individual = self.w_q1 * self.sp * self.sm
+        self.H_q2_individual = self.w_q2 * self.sp * self.sm
+        self.H_q1 = tensor(self.H_q1_individual, self.Iq, self.Ivib, self.Ivib, self.Icav)
+        self.H_q2 = tensor(self.Iq, self.H_q2_individual, self.Ivib, self.Ivib, self.Icav)
+        self.H_qubit = self.H_q1 + self.H_q2
+
+    def build_vibrational_hamiltonians(self):
+        """ Method to build the vibrational Hamiltonians for both modes on the full Hilbert space."""
+        self.H_vib1_individual = self.w_vib1 * (self.ap * self.a + 0.0)
+        self.H_vib2_individual = self.w_vib2 * (self.ap * self.a + 0.0)
+        self.H_vib1 = tensor(self.Iq, self.Iq, self.H_vib1_individual, self.Ivib, self.Icav)
+        self.H_vib2 = tensor(self.Iq, self.Iq, self.Ivib, self.H_vib2_individual, self.Icav)
+        self.H_vibrational = self.H_vib1 + self.H_vib2
+
+    def build_cavity_hamiltonian(self):
+        """ Method to build the cavity Hamiltonian on the full Hilbert space."""
+        self.H_cav_individual = self.w_cav * (self.bp * self.b + 0.0)
+        self.H_cav = tensor(self.Iq, self.Iq, self.Ivib, self.Ivib, self.H_cav_individual)
+
+    def build_qubit_vibrational_coupling(self):
+        """ Method to build the qubit-vibrational coupling Hamiltonians on the full Hilbert space."""
+
+        # first build the individual coupling terms
+        _qubit_excitation = self.sp * self.sm
+        _vib_excitation = self.ap + self.a
+
+        # qubit 1 coupling constant is squared Huang-Rhys factor
+        _lambda_1 = np.sqrt(self.S_1) 
+        _lambda_2 = np.sqrt(self.S_2)
+
+        # now scale by the Huang-Rhys factors and build on the full Hilbert space
+        self.H_q1_vib1 = _lambda_1 * tensor(_qubit_excitation, self.Iq, _vib_excitation, self.Ivib, self.Icav)
+        self.H_q2_vib2 = _lambda_2 * tensor(self.Iq, _qubit_excitation, self.Ivib, _vib_excitation, self.Icav)
+        self.H_qubit_vibrational_coupling = self.H_q1_vib1 + self.H_q2_vib2
+
+    def build_qubit_cavity_coupling(self, pauli_fierz=False, dse_term=False, simple_g = False):
+        """ Method to build the qubit-cavity bilinear coupling Hamiltonians on the full Hilbert space.
+
+        Args:
+            pauli_fierz (bool): If True, use the Pauli-Fierz form of the coupling.
+
+            if False, use Tavis-Cummings form of the coupling.
+            Default is False.
+        """
+        if pauli_fierz:
+            # Pauli-Fierz coupling - needs the dipole operator
+            if not hasattr(self, 'dipole_operator'):
+                self.build_qubit_dipole_operator()
+
+            _cavity_excitation = self.b + self.bp
+            _qubit_1_d_operator = np.sqrt(self.w_q1/2) * self.lambda_1 * self.dipole_operator_q1
+            _qubit_2_d_operator = np.sqrt(self.w_q2/2) * self.lambda_2 * self.dipole_operator_q2
+            self.H_q1_cav = tensor(_qubit_1_d_operator, self.Iq, self.Ivib, self.Ivib, _cavity_excitation)
+            self.H_q2_cav = tensor(self.Iq, _qubit_2_d_operator, self.Ivib, self.Ivib, _cavity_excitation)
+            self.H_qubit_cavity_coupling = self.H_q1_cav + self.H_q2_cav
+
+            if dse_term:
+                _d1 = self.lambda_1 * self.dipole_operator_q1
+                _d2 = self.lambda_2 * self.dipole_operator_q2
+                d1 = tensor(_d1, self.Iq, self.Ivib, self.Ivib, self.Icav)
+                d2 = tensor(self.Iq, _d2, self.Ivib, self.Ivib, self.Icav)
+                self.H_dipole_self_energy = 0.5 * (d1 + d2) @ (d1 + d2)
+
+        else:
+            print("Using Tavis-Cummings coupling")
+            # Tavis-Cummings coupling - only TDM element needed
+            if not hasattr(self, 'qubit_1_dipole_operator'):
+                self.build_qubit_dipole_operator()
+            if simple_g:
+                print("Using simple g coupling")
+                _g1 = self.g1
+                _g2 = self.g2
+                print(f"g1: {self.g1}, g2: {self.g2}")
+            else:
+                print("Using g coupling with TDM")
+                _g1 = np.sqrt(self.w_q1/2) * self.lambda_1 * self.qubit_1_dipole_operator[0,1]
+                _g2 = np.sqrt(self.w_q2/2) * self.lambda_2 * self.qubit_2_dipole_operator[0,1]
+
+            _t1a = tensor(_g1 * self.sp, self.Iq, self.Ivib, self.Ivib, self.b)
+            _t1b = tensor(_g1 * self.sm, self.Iq, self.Ivib, self.Ivib, self.bp)
+            _t2a = tensor(self.Iq, _g2 * self.sp, self.Ivib, self.Ivib, self.b)
+            _t2b = tensor(self.Iq, _g2 * self.sm, self.Ivib, self.Ivib, self.bp)
+
+            self.H_q1_cav = _t1a + _t1b
+            self.H_q2_cav = _t2a + _t2b
+            self.H_qubit_cavity_coupling = self.H_q1_cav + self.H_q2_cav
+
+
+    def build_hamiltonian(self):
+        self.build_qubit_hamiltonians()
+        self.build_vibrational_hamiltonians()
+        self.build_cavity_hamiltonian()
+        self.build_qubit_vibrational_coupling()
+        self.build_qubit_cavity_coupling(self.pauli_fierz, self.dse_term, self.simple_g)
+        # Combine all parts to form the total Hamiltonian
+        self.H_total = self.H_qubit + self.H_vibrational + self.H_cav + self.H_qubit_vibrational_coupling + self.H_qubit_cavity_coupling
+        self.eigenvalues, self.eigenstates = self.H_total.eigenstates()
+
+
+
+    
 
